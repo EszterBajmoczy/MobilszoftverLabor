6e331c447c871183106b10e891f254b6441a76ef
Bajmóczy Eszter Csilla <36538209+EszterBajmoczy@users.noreply.github.com>
2022-04-30 22:20:00 +0200

    Add day details files

---
+++ app/src/main/java/hu/bme/aut/eonvis/ui/dayDetails/DayDetailsActivity.kt
@@ -0,0 +1,12 @@
+package hu.bme.aut.eonvis.ui.dayDetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.bme.aut.eonvis.R
+
+class DayDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_day_details)
+    }
+}
